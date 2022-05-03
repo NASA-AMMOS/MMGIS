58f7cece708ca35ec1ed7bbda8953f4f7eb6d73d
Tariq Soliman <Tariq.K.Soliman@jpl.nasa.gov>
2022-05-03 15:55:12 -0700

    Audit fix and add analyzer

--- configuration/webpack.config.js
+++ configuration/webpack.config.js
@@ -24,6 +24,8 @@ const getClientEnvironment = require("./env");
 const ModuleNotFoundPlugin = require("react-dev-utils/ModuleNotFoundPlugin");
 const ForkTsCheckerWebpackPlugin = require("react-dev-utils/ForkTsCheckerWebpackPlugin");
 const typescriptFormatter = require("react-dev-utils/typescriptFormatter");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 const postcssNormalize = require("postcss-normalize");
 
@@ -55,6 +57,9 @@ const sassModuleRegex = /\.module\.(scss|sass)$/;
 module.exports = function (webpackEnv) {
   const isEnvDevelopment = webpackEnv === "development";
   const isEnvProduction = webpackEnv === "production";
+
+  const isEnvDevAnalyze =
+    isEnvDevelopment && process.argv.includes("--analyze");
 
   // Variable used for enabling profiling in Production
   // passed into alias object. Uses a flag if passed into the build command
@@ -663,6 +668,7 @@ module.exports = function (webpackEnv) {
           // The formatter is invoked directly in WebpackDevServerUtils during development
           formatter: isEnvProduction ? typescriptFormatter : undefined,
         }),
+      isEnvDevAnalyze && new BundleAnalyzerPlugin(),
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell webpack to provide empty mocks for them so importing them works.
