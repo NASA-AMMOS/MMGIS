903907c8efe4b92e1ff4573168ad41e94c86f2fc
tariqksoliman <tariqksoliman@gmail.com>
2022-05-10 17:29:24 -0700

    #180 Hide login fields when not visible

--- src/essence/Ancillary/Login/Login.js
+++ src/essence/Ancillary/Login/Login.js
@@ -83,6 +83,160 @@ var Login = {
             .style('color', '#aaa')
             .style('mix-blend-mode', 'luminosity')
 
+        Login.loginBar
+            .append('div')
+            .attr('id', 'loginUser')
+            .attr('title', Login.loggedIn ? Login.username : '')
+            .style('text-align', 'center')
+            .style('font-size', '12px')
+            .style('font-weight', 'bold')
+            .style('font-family', 'sans-serif')
+            .style('margin-left', '5px')
+            .style('cursor', 'pointer')
+            .style('width', '30px')
+            .style('height', '30px')
+            .style('line-height', '30px')
+            .style('color', 'white')
+            .style(
+                'background',
+                Login.loggedIn ? 'var(--color-a)' : 'transparent'
+            )
+            .style('display', Login.loggedIn ? 'block' : 'none')
+            .style('text-transform', 'uppercase')
+            .style('transition', 'opacity 0.2s ease-out')
+            .html(Login.loggedIn ? Login.username[0] : '')
+
+        //Show signup for admins
+        if (
+            window.mmgisglobal.AUTH === 'local' &&
+            window.mmgisglobal.permission === '111'
+        ) {
+            Login.loginBar
+                .append('div')
+                .attr('id', 'forceSignupButton')
+                .style('text-align', 'center')
+                .style('cursor', 'pointer')
+                .style('width', '30px')
+                .style('height', '30px')
+                .style('line-height', '26px')
+                .style('margin-right', '4px')
+                .style('background', 'var(--color-a)')
+                .style('pointer-events', 'all')
+                .on('click', function () {
+                    //Open login
+                    //default to signup
+                    Login.signUp = true
+                    $('#loginEmail').css({ display: 'inherit' })
+                    $('#loginRetypePassword').css({ display: 'inherit' })
+                    $('#loginSubmit').val('Sign Up')
+                    $('#loginInUpToggle').html('or sign in')
+                    //and open modal
+                    Login.openModal()
+                })
+                .append('i')
+                .attr('id', 'forceSignupButtonIcon')
+                .attr('class', 'mdi mdi-account-multiple mdi-18px')
+        }
+
+        Login.loginBar
+            .append('div')
+            .attr('id', 'loginoutButton')
+            .attr('title', Login.loggedIn ? 'Logout' : 'Login')
+            .attr('tabindex', 500)
+            .on('click', function () {
+                if (Login.loggedIn) {
+                    //Then Logout
+                    if (Login.beganLoggedIn) {
+                        Login.loggedIn = false
+                        window.location.href = '/ssologoutredirect'
+                    } else {
+                        calls.api(
+                            'logout',
+                            { username: Login.username },
+                            function (d) {
+                                ToolController_.closeActiveTool()
+                                window.mmgisglobal.user = 'guest'
+                                window.mmgisglobal.groups = []
+
+                                Login.username = null
+                                Login.loggedIn = false
+                                d3.select('#loginUser')
+                                    .style('display', 'none')
+                                    .html('')
+                                d3.select('#loginoutButton').attr(
+                                    'title',
+                                    'Login'
+                                )
+                                d3.select('#loginoutButtonIcon').attr(
+                                    'class',
+                                    'mdi mdi-login mdi-18px'
+                                )
+                                // Destroy the cookie session here
+                                var decodedCookie = decodeURIComponent(
+                                    document.cookie
+                                )
+                                var cookies = decodedCookie.split(';')
+                                var MMGISUser = {}
+                                try {
+                                    MMGISUser = cookies[0].split('=')
+                                    MMGISUser = JSON.parse(MMGISUser[1])
+                                } catch (err) {}
+                                MMGISUser.username = ''
+                                MMGISUser.token = ''
+
+                                if (window.mmgisglobal.AUTH === 'local') {
+                                    window.location.reload()
+                                }
+                            },
+                            function (d) {}
+                        )
+                    }
+                } else {
+                    //Open login
+                    //default to login
+                    Login.signUp = false
+                    $('#loginEmail').css({ display: 'none' })
+                    $('#loginRetypePassword').css({ display: 'none' })
+                    $('#loginSubmit').val('Sign In')
+                    $('#loginInUpToggle').html('or sign up')
+                    //and open modal
+                    Login.openModal()
+                }
+            })
+            .append('i')
+            .attr('id', 'loginoutButtonIcon')
+            .attr(
+                'class',
+                Login.loggedIn
+                    ? 'mdi mdi-logout mdi-18px'
+                    : 'mdi mdi-login mdi-18px'
+            )
+
+        //Sign in at page load from cookie if possible
+        if (
+            window.mmgisglobal.AUTH !== 'off' &&
+            window.mmgisglobal.AUTH !== 'csso'
+        ) {
+            calls.api(
+                'login',
+                {
+                    useToken: true,
+                },
+                function (d) {
+                    Login.username = d.username
+                    window.mmgisglobal.user = Login.username
+                    window.mmgisglobal.groups = d.groups
+                    loginSuccess(d)
+                },
+                function (d) {
+                    loginSuccess(d, true)
+                }
+            )
+        }
+    },
+    createModal: function () {
+        this.removeModal()
+
         const wrapper = d3
             .select('#topBar')
             .append('div')
@@ -245,13 +399,13 @@ var Login = {
                                 $('#loginErrorMessage').animate({ opacity: '0' }, 500)
                                 // prettier-ignore
                                 $('#loginModal').parent()
-                                        .animate({'background-color': 'rgba(46,180,255,0.6)'}, 1000,
-                                            function() {
-                                                setTimeout(function() {
-                                                    $('#loginModal').parent().css({'background-color':'rgba(0,0,0,0.6)'})
-                                                }, 2000 )
-                                            }
-                                        )
+                                    .animate({'background-color': 'rgba(46,180,255,0.6)'}, 1000,
+                                        function() {
+                                            setTimeout(function() {
+                                                $('#loginModal').parent().css({'background-color':'rgba(0,0,0,0.6)'})
+                                            }, 2000 )
+                                        }
+                                    )
                             } else {
                                 Login.username = d.username
                                 window.mmgisglobal.user = Login.username
@@ -287,157 +441,6 @@ var Login = {
             return false
         })
 
-        Login.loginBar
-            .append('div')
-            .attr('id', 'loginUser')
-            .attr('title', Login.loggedIn ? Login.username : '')
-            .style('text-align', 'center')
-            .style('font-size', '12px')
-            .style('font-weight', 'bold')
-            .style('font-family', 'sans-serif')
-            .style('margin-left', '5px')
-            .style('cursor', 'pointer')
-            .style('width', '30px')
-            .style('height', '30px')
-            .style('line-height', '30px')
-            .style('color', 'white')
-            .style(
-                'background',
-                Login.loggedIn ? 'var(--color-a)' : 'transparent'
-            )
-            .style('display', Login.loggedIn ? 'block' : 'none')
-            .style('text-transform', 'uppercase')
-            .style('transition', 'opacity 0.2s ease-out')
-            .html(Login.loggedIn ? Login.username[0] : '')
-
-        //Show signup for admins
-        if (
-            window.mmgisglobal.AUTH === 'local' &&
-            window.mmgisglobal.permission === '111'
-        ) {
-            Login.loginBar
-                .append('div')
-                .attr('id', 'forceSignupButton')
-                .style('text-align', 'center')
-                .style('cursor', 'pointer')
-                .style('width', '30px')
-                .style('height', '30px')
-                .style('line-height', '26px')
-                .style('margin-right', '4px')
-                .style('background', 'var(--color-a)')
-                .style('pointer-events', 'all')
-                .on('click', function () {
-                    //Open login
-                    //default to signup
-                    Login.signUp = true
-                    $('#loginEmail').css({ display: 'inherit' })
-                    $('#loginRetypePassword').css({ display: 'inherit' })
-                    $('#loginSubmit').val('Sign Up')
-                    $('#loginInUpToggle').html('or sign in')
-                    //and open modal
-                    Login.openModal()
-                })
-                .append('i')
-                .attr('id', 'forceSignupButtonIcon')
-                .attr('class', 'mdi mdi-account-multiple mdi-18px')
-        }
-
-        Login.loginBar
-            .append('div')
-            .attr('id', 'loginoutButton')
-            .attr('title', Login.loggedIn ? 'Logout' : 'Login')
-            .attr('tabindex', 500)
-            .on('click', function () {
-                if (Login.loggedIn) {
-                    //Then Logout
-                    if (Login.beganLoggedIn) {
-                        Login.loggedIn = false
-                        window.location.href = '/ssologoutredirect'
-                    } else {
-                        calls.api(
-                            'logout',
-                            { username: Login.username },
-                            function (d) {
-                                ToolController_.closeActiveTool()
-                                window.mmgisglobal.user = 'guest'
-                                window.mmgisglobal.groups = []
-
-                                Login.username = null
-                                Login.loggedIn = false
-                                d3.select('#loginUser')
-                                    .style('display', 'none')
-                                    .html('')
-                                d3.select('#loginoutButton').attr(
-                                    'title',
-                                    'Login'
-                                )
-                                d3.select('#loginoutButtonIcon').attr(
-                                    'class',
-                                    'mdi mdi-login mdi-18px'
-                                )
-                                // Destroy the cookie session here
-                                var decodedCookie = decodeURIComponent(
-                                    document.cookie
-                                )
-                                var cookies = decodedCookie.split(';')
-                                var MMGISUser = {}
-                                try {
-                                    MMGISUser = cookies[0].split('=')
-                                    MMGISUser = JSON.parse(MMGISUser[1])
-                                } catch (err) {}
-                                MMGISUser.username = ''
-                                MMGISUser.token = ''
-
-                                if (window.mmgisglobal.AUTH === 'local') {
-                                    window.location.reload()
-                                }
-                            },
-                            function (d) {}
-                        )
-                    }
-                } else {
-                    //Open login
-                    //default to login
-                    Login.signUp = false
-                    $('#loginEmail').css({ display: 'none' })
-                    $('#loginRetypePassword').css({ display: 'none' })
-                    $('#loginSubmit').val('Sign In')
-                    $('#loginInUpToggle').html('or sign up')
-                    //and open modal
-                    Login.openModal()
-                }
-            })
-            .append('i')
-            .attr('id', 'loginoutButtonIcon')
-            .attr(
-                'class',
-                Login.loggedIn
-                    ? 'mdi mdi-logout mdi-18px'
-                    : 'mdi mdi-login mdi-18px'
-            )
-
-        //Sign in at page load from cookie if possible
-        if (
-            window.mmgisglobal.AUTH !== 'off' &&
-            window.mmgisglobal.AUTH !== 'csso'
-        ) {
-            calls.api(
-                'login',
-                {
-                    useToken: true,
-                },
-                function (d) {
-                    Login.username = d.username
-                    window.mmgisglobal.user = Login.username
-                    window.mmgisglobal.groups = d.groups
-                    loginSuccess(d)
-                },
-                function (d) {
-                    loginSuccess(d, true)
-                }
-            )
-        }
-
         $('#loginModal').on('click', function (e) {
             // stop the event from bubbling.
             e.stopPropagation()
@@ -448,10 +451,19 @@ var Login = {
         })
     },
     openModal: function () {
-        $('#loginModalWrapper').addClass('active')
+        this.createModal()
+        setTimeout(() => {
+            $('#loginModalWrapper').addClass('active')
+        }, 100)
     },
     closeModal: function () {
         $('#loginModalWrapper').removeClass('active')
+        setTimeout(() => {
+            Login.removeModal()
+        }, 250)
+    },
+    removeModal: function () {
+        $('#topBar > #loginModalWrapper').remove()
     },
 }
 
