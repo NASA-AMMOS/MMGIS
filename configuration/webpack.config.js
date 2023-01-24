e870f6946c5b425fcf01dae262852a081ddbaf34
Tariq Soliman <Tariq.K.Soliman@jpl.nasa.gov>
2023-01-23 18:08:50 -0800

    Fix Node 18+ build

--- configuration/webpack.config.js
+++ configuration/webpack.config.js
@@ -31,6 +31,14 @@ const postcssNormalize = require("postcss-normalize");
 
 const appPackageJson = require(paths.appPackageJson);
 
+// This is a hack so that node v18+ can run.
+// "md4" hashes are no longer supported so before everything runs, we override them to be "sha256"
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = (algorithm) =>
+  crypto_orig_createHash(algorithm === "md4" ? "sha256" : algorithm);
+// end hack
+
 // Source maps are resource heavy and can cause out of memory issue for large source files.
 const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== "false";
 // Some apps do not need the benefits of saving a web request, so not inlining the chunk
