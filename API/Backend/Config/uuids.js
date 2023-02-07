edc751caba890052cc3c1ef7f050c74adbb01ece
achung <achung@jpl.nasa.gov>
2023-02-07 14:35:28 -0800

    Make sure user input UUIDs through REST API are unique

--- API/Backend/Config/uuids.js
+++ API/Backend/Config/uuids.js
@@ -4,6 +4,7 @@ const { v4: uuidv4, validate: uuidValidate } = require("uuid");
 
 const populateUUIDs = (config) => {
   const newlyAddedUUIDs = [];
+  const definedUUIDs = [];
 
   Utils.traverseLayers(config.layers, (layer) => {
     if (layer.uuid == null) {
@@ -12,7 +13,7 @@ const populateUUIDs = (config) => {
         name: layer.name,
         uuid: layer.uuid,
       });
-    } else if (!uuidValidate(layer.uuid)) {
+    } else if (!uuidValidate(layer.uuid) || definedUUIDs.includes(layer.uuid)) {
       const badUUID = layer.uuid;
       layer.uuid = uuidv4();
       newlyAddedUUIDs.push({
@@ -20,6 +21,8 @@ const populateUUIDs = (config) => {
         uuid: layer.uuid,
         replacesBadUUID: badUUID,
       });
+    } else {
+      definedUUIDs.push(layer.uuid)
     }
   });
   return newlyAddedUUIDs;
