8309f00173508493cb236fee3ed171f55a5acc01
tariqksoliman <tariqksoliman@gmail.com>
2024-04-04 16:41:12 -0700

    #525 Make dynamicExtent requery timeEnabled aware.

--- src/essence/Basics/Map_/Map_.js
+++ src/essence/Basics/Map_/Map_.js
@@ -784,10 +784,11 @@ async function makeVectorLayer(
                 add,
                 (f) => {
                     Map_.map.on('moveend', f)
-                    L_.subscribeTimeChange(
-                        `dynamicgeodataset_${layerObj.name}`,
-                        f
-                    )
+                    if (layerObj.time?.enabled === true)
+                        L_.subscribeTimeChange(
+                            `dynamicgeodataset_${layerObj.name}`,
+                            f
+                        )
                     L_.subscribeOnSpecificLayerToggle(
                         `dynamicgeodataset_${layerObj.name}`,
                         layerObj.name,
