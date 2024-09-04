c81f251652de5f7d6d9f89793be6828039356299
Amanda Chung <Amanda.Chung@jpl.nasa.gov>
2024-09-04 15:23:09 -0700

    Add Image layer type

--- API/Backend/Config/validate.js
+++ API/Backend/Config/validate.js
@@ -86,6 +86,12 @@ const validateLayers = (config) => {
         // Check model params (pos, rot, scale)
         errs = errs.concat(isValidModelParams(layer));
         break;
+      case "image":
+        // Check url
+        errs = errs.concat(isValidUrl(layer));
+        // Check zooms
+        errs = errs.concat(isValidZooms(layer));
+        break;
       default:
         errs = errs.concat(
           err(`Unknown layer type: '${layer.type}'`, ["layers[layer].type"])
@@ -310,6 +316,10 @@ const fillInMissingFieldsWithDefaults = (layer) => {
       layer.style.className = layer.name.replace(/ /g, "").toLowerCase();
       break;
     case "vector":
+      layer.style = layer.style || {};
+      layer.style.className = layer.name.replace(/ /g, "").toLowerCase();
+      break;
+    case "image":
       layer.style = layer.style || {};
       layer.style.className = layer.name.replace(/ /g, "").toLowerCase();
       break;
