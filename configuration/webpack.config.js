4063aa1d541f2a6384f95df2f2ce4cfa6d598f28
achung <achung@jpl.nasa.gov>
2024-09-26 11:10:34 -0700

    Try georaster-layer-for-leaflet v4.1.1-0

--- configuration/webpack.config.js
+++ configuration/webpack.config.js
@@ -381,7 +381,7 @@ module.exports = function (webpackEnv) {
             // Process any JS outside of the app with Babel.
             // Unlike the application JS, we only compile the standard ES features.
             {
-              test: /\.(js|mjs)$/,
+              test: /\.(js|mjs|cjs)$/,
               exclude: /@babel(?:\/|\\{1,2})runtime/,
               loader: require.resolve("babel-loader"),
               options: {
