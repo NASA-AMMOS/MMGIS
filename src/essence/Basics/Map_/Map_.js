dc9d9fd8023ac7e78b3b9d065b4985b868f8a674
achung <achung@jpl.nasa.gov>
2024-10-03 15:44:14 -0700

    Do not fetch entire COG file before loading

--- src/essence/Basics/Map_/Map_.js
+++ src/essence/Basics/Map_/Map_.js
@@ -1162,76 +1162,19 @@ function makeImageLayer(layerObj) {
         )
     }
 
+    parseGeoraster(layerUrl).then((georaster) => {
+        console.log("layerObj.display_name", layerObj.display_name)
+        L_.layers.layer[layerObj.name] = new GeoRasterLayer({
+            georaster: georaster,
+            resolution: 256,
+            debugLevel: 1,
+        });
 
-    console.log("layerUrl", layerUrl)
-
-        layerUrl = 'http://localhost:8889/' + layerUrl;
-            parseGeoraster(layerUrl).then((georaster) => {
-                console.log("georaster:", georaster);
-
-                console.log("layerObj.display_name", layerObj.display_name)
-                L_.layers.layer[layerObj.name] = new GeoRasterLayer({
-                    georaster: georaster,
-                    resolution: 256, //128
-
-/*
-                    pixelValuesToColorFn: (values) => {
-                        if (georaster.mins.length === 1) {
-
-                        }
-                        // https://github.com/GeoTIFF/georaster-layer-for-leaflet/issues/16
-                        //return values[0] === georaster.noDataValue ? null : `rgb(${values[0]},${values[1]},${values[2]})`
-                        if (values[0] === georaster.noDataValue) {
-                            console.log("value", `rgb(${values[0]},${values[1]},${values[2]})`)
-                        }
-                        return values[0] === georaster.noDataValue ? null : `rgb(${values[0]},${values[1]},${values[2]})`
-                    },
-*/
-                    debugLevel: 1,
-                });
-
-                L_.setLayerOpacity(layerObj.name, L_.layers.opacity[layerObj.name])
-
-
-            });
-
-                L_._layersLoaded[L_._layersOrdered.indexOf(layerObj.name)] = true
-                allLayersLoaded()
-
-/*
-    fetch(layerUrl)
-        .then(response => response.arrayBuffer())
-        .then(arrayBuffer => {
-            parseGeoraster(arrayBuffer, { readOnDemand: true }).then((georaster) => {
-                console.log("georaster:", georaster);
-
-                console.log("layerObj.display_name", layerObj.display_name)
-                L_.layers.layer[layerObj.name] = new GeoRasterLayer({
-                    georaster: georaster,
-                    resolution: 256, //128
-
-                    pixelValuesToColorFn: (values) => {
-                        if (georaster.mins.length === 1) {
-
-                        }
-                        // https://github.com/GeoTIFF/georaster-layer-for-leaflet/issues/16
-                        //return values[0] === georaster.noDataValue ? null : `rgb(${values[0]},${values[1]},${values[2]})`
-                        if (values[0] === georaster.noDataValue) {
-                            console.log("value", `rgb(${values[0]},${values[1]},${values[2]})`)
-                        }
-                        return values[0] === georaster.noDataValue ? null : `rgb(${values[0]},${values[1]},${values[2]})`
-                    },
-                    debugLevel: 1,
-                });
-
-                L_.setLayerOpacity(layerObj.name, L_.layers.opacity[layerObj.name])
-
-                L_._layersLoaded[L_._layersOrdered.indexOf(layerObj.name)] = true
-                allLayersLoaded()
-            });
-
+        L_.setLayerOpacity(layerObj.name, L_.layers.opacity[layerObj.name])
     });
-*/
+
+    L_._layersLoaded[L_._layersOrdered.indexOf(layerObj.name)] = true
+    allLayersLoaded()
 }
 
 
