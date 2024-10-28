532656e390e31b2a14ca58327c566c2ab218fc73
Tariq Soliman <Tariq.K.Soliman@jpl.nasa.gov>
2024-10-28 15:44:03 -0700

    Update adjacent-servers.js

--- adjacent-servers/adjacent-servers.js
+++ adjacent-servers/adjacent-servers.js
@@ -4,29 +4,30 @@ const { spawn } = require("child_process");
 
 const IS_WINDOWS = /^win/i.test(process.platform) ? true : false;
 const EXT = IS_WINDOWS ? ".bat" : ".sh";
+const CMD = IS_WINDOWS ? "" : "sh ";
 
 const adjacentServers = [
   {
     name: "stac",
-    command: `cd adjacent-servers/stac/ && start-stac${EXT}`,
+    command: `cd adjacent-servers/stac/ && ${CMD}start-stac${EXT}`,
     port: process.env.STAC_PORT || 8881,
     env: "WITH_STAC",
   },
   {
     name: "tipg",
-    command: `cd adjacent-servers/tipg/ && start-tipg${EXT}`,
+    command: `cd adjacent-servers/tipg/ && ${CMD}start-tipg${EXT}`,
     port: process.env.TIPG_PORT || 8882,
     env: "WITH_TIPG",
   },
   {
     name: "titiler",
-    command: `cd adjacent-servers/titiler/ && start-titiler${EXT}`,
+    command: `cd adjacent-servers/titiler/ && ${CMD}start-titiler${EXT}`,
     port: process.env.TITILER_PORT || 8883,
     env: "WITH_TITILER",
   },
   {
     name: "titiler-pgstac",
-    command: `cd adjacent-servers/titiler-pgstac/ && start-titiler-pgstac${EXT}`,
+    command: `cd adjacent-servers/titiler-pgstac/ && ${CMD}start-titiler-pgstac${EXT}`,
     port: process.env.TITILER_PGSTAC_PORT || 8884,
     env: "WITH_TITILER_PGSTAC",
   },
