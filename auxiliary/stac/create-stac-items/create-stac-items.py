45b9518c80e597595ce94f5f004e40ac9a13ccb7
Tariq Soliman <Tariq.K.Soliman@jpl.nasa.gov>
2024-12-09 18:07:39 -0800

    Fix create-stac-items.py to be able to push to custom internal docker paths

--- auxiliary/stac/create-stac-items/create-stac-items.py
+++ auxiliary/stac/create-stac-items/create-stac-items.py
@@ -1,6 +1,6 @@
 # Developed on python 3.11.4
 # python create-stac-items.py [mmgis_url] [mmgis_token] [collection_id] [file_or_folder_path] [--regex]
-# ex: python create-stac-items.py "http://localhost:8888" stac-201fb492061f9fe3575e18f76d8d3823 myCollection C:\Users\Documents\Projects\MMGIS\Missions\Earth\COGs
+# ex: python create-stac-items.py "http://localhost:8888" stac-201fb492061f9fe3575e18f76d8d3823 myCollection C:\Users\Documents\Projects\MMGIS\Missions\Earth\COGs --upsert --path_remove /var/www/html/mmgis/MMGIS/ --path_replace_with /
 
 
 import requests
@@ -17,6 +17,8 @@ def parse_args():
     parser.add_argument('mmgis_token', help='MMGIS API Token')
     parser.add_argument('collection_id', help='Pre-existing STAC collection id')
     parser.add_argument('file_or_folder_path', help='Input file or folder path')
+    parser.add_argument('-rm', '--path_remove', help='Portion of internal file path to remove. Useful if the current filepath differs from what titiler should hit internally.')
+    parser.add_argument('-rp', '--path_replace_with', help='If --path_remove, instead of setting that portion of the internal path to "", replaces it with this value.')
     parser.add_argument('-u', '--upsert', help='Allow overwriting existing STAC items', action='store_true')
     parser.add_argument('-r', '--regex', help='If folder, only create stac items for files that match this regex')
     parser.add_argument('-d', '--dev', help='dev mode that does other things', action='store_true')
@@ -24,7 +26,7 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-def create_stac_items(mmgis_url, mmgis_token, collection_id, file_or_folder_path, upsert=False, regex=None, dev=False):
+def create_stac_items(mmgis_url, mmgis_token, collection_id, file_or_folder_path, path_remove, path_replace_with, upsert=False, regex=None, dev=False):
 
     isDir = os.path.isdir(file_or_folder_path)
     
@@ -50,6 +52,13 @@ def create_stac_items(mmgis_url, mmgis_token, collection_id, file_or_folder_path
 
     for idx, file in enumerate(files, start=1):
         print(f'Gathering metadata {idx}/{len(files)}...', end='\r', flush=True)
+        asset_href = file
+        if path_remove is not None:
+            if path_replace_with is not None:
+                asset_href = file.replace(path_remove, path_replace_with)
+            else:
+                asset_href = file.replace(path_remove, "")
+
         if dev:
             item = create_stac_item(
                 file,
@@ -60,7 +69,7 @@ def create_stac_items(mmgis_url, mmgis_token, collection_id, file_or_folder_path
                 #properties=property,
                 #id=id,
                 #asset_name=asset_name,
-                #asset_href=asset_href,
+                asset_href=asset_href,
                 #asset_media_type=asset_mediatype,
                 with_proj=True,
                 with_raster=True,
@@ -79,7 +88,7 @@ def create_stac_items(mmgis_url, mmgis_token, collection_id, file_or_folder_path
                 #properties=property,
                 #id=id,
                 #asset_name=asset_name,
-                #asset_href=asset_href,
+                asset_href=asset_href,
                 #asset_media_type=asset_mediatype,
                 with_proj=True,
                 with_raster=True,
@@ -112,5 +121,5 @@ def create_stac_items(mmgis_url, mmgis_token, collection_id, file_or_folder_path
 
 if __name__ == '__main__':
     args = parse_args()
-    create_stac_items(args.mmgis_url, args.mmgis_token, args.collection_id, args.file_or_folder_path, args.upsert, args.regex, args.dev)
+    create_stac_items(args.mmgis_url, args.mmgis_token, args.collection_id, args.file_or_folder_path, args.path_remove, args.path_replace_with, args.upsert, args.regex, args.dev)
     exit()
