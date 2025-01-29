caa7e1af2ac651cf8afe158af5cff1522803ecac
tariqksoliman <tariqksoliman@gmail.com>
2025-01-28 18:29:41 -0800

    #622 Fix docker-compose depends_on

--- API/Backend/Utils/routes/utils.js
+++ API/Backend/Utils/routes/utils.js
@@ -6,6 +6,9 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const path = require("path");
+const exec = require("child_process").exec;
+const execFile = require("child_process").execFile;
+
 const Sequelize = require("sequelize");
 const { sequelizeSTAC } = require("../../../connection");
 
@@ -225,6 +228,147 @@ function queryTilesetTimesStac(req, res) {
 router.get("/queryTilesetTimes", function (req, res) {
   if (req.query.stacCollection != null) queryTilesetTimesStac(req, res);
   else queryTilesetTimesDir(req, res);
+});
+
+// API
+// TODO: move to API/Backend
+//TEST
+router.get("/healthcheck", function (req, res) {
+  res.send("Alive and Well!");
+});
+
+// TODO: Remove or move to Setup structure. Some are definitely still used.
+
+//utils getprofile
+router.post("/getprofile", function (req, res) {
+  const path = encodeURIComponent(req.body.path);
+  const lat1 = encodeURIComponent(req.body.lat1);
+  const lon1 = encodeURIComponent(req.body.lon1);
+  const lat2 = encodeURIComponent(req.body.lat2);
+  const lon2 = encodeURIComponent(req.body.lon2);
+  const steps = encodeURIComponent(req.body.steps);
+  const axes = encodeURIComponent(req.body.axes);
+
+  execFile(
+    "python",
+    [
+      "private/api/2ptsToProfile.py",
+      path,
+      lat1,
+      lon1,
+      lat2,
+      lon2,
+      steps,
+      axes,
+      1,
+    ],
+    function (error, stdout, stderr) {
+      if (error) {
+        logger("warn", error);
+        res.status(400).send();
+      } else {
+        res.send(stdout.replace(/None/g, null));
+      }
+    }
+  );
+});
+
+//utils getbands
+router.post("/getbands", function (req, res) {
+  const path = encodeURIComponent(req.body.path);
+  const x = encodeURIComponent(req.body.x);
+  const y = encodeURIComponent(req.body.y);
+  const xyorll = encodeURIComponent(req.body.xyorll);
+  const bands = encodeURIComponent(req.body.bands);
+
+  execFile(
+    "python",
+    ["private/api/BandsToProfile.py", path, x, y, xyorll, bands],
+    function (error, stdout, stderr) {
+      if (error) {
+        logger("warn", error);
+        res.status(400).send();
+      } else {
+        res.send(stdout);
+      }
+    }
+  );
+});
+
+//utils ll2aerll
+router.post("/ll2aerll", function (req, res) {
+  const lng = encodeURIComponent(req.body.lng);
+  const lat = encodeURIComponent(req.body.lat);
+  const height = encodeURIComponent(req.body.height);
+  const target = encodeURIComponent(req.body.target);
+  const time = encodeURIComponent(req.body.time)
+    .replace(/%20/g, " ")
+    .replace(/%3A/g, ":");
+  const obsRefFrame = encodeURIComponent(req.body.obsRefFrame) || "IAU_MARS";
+  const obsBody = encodeURIComponent(req.body.obsBody) || "MARS";
+  const includeSunEarth =
+    encodeURIComponent(req.body.includeSunEarth) || "False";
+
+  const isCustom = encodeURIComponent(req.body.isCustom) || "False";
+  const customAz = encodeURIComponent(req.body.customAz);
+  const customEl = encodeURIComponent(req.body.customEl);
+  const customRange = encodeURIComponent(req.body.customRange);
+
+  execFile(
+    "python",
+    [
+      "private/api/ll2aerll.py",
+      lng,
+      lat,
+      height,
+      target,
+      time,
+      obsRefFrame,
+      obsBody,
+      includeSunEarth,
+      isCustom,
+      customAz,
+      customEl,
+      customRange,
+    ],
+    function (error, stdout, stderr) {
+      if (error) logger("error", "ll2aerll failure:", "server", null, error);
+      res.send(stdout);
+    }
+  );
+});
+
+//utils chronos (spice time converter)
+router.post("/chronice", function (req, res) {
+  const body = encodeURIComponent(req.body.body);
+  const target = encodeURIComponent(req.body.target);
+  const fromFormat = encodeURIComponent(req.body.from);
+  const time = encodeURIComponent(req.body.time)
+    .replace(/%20/g, " ")
+    .replace(/%3A/g, ":");
+
+  execFile(
+    "python",
+    ["private/api/chronice.py", body, target, fromFormat, time],
+    function (error, stdout, stderr) {
+      if (error) logger("error", "chronice failure:", "server", null, error);
+      res.send(stdout);
+    }
+  );
+});
+
+//utils chronos (spice time converter)
+router.get("/proj42wkt", function (req, res) {
+  const proj4 = encodeURIComponent(req.query.proj4);
+
+  execFile(
+    "python",
+    ["private/api/proj42wkt.py", proj4],
+    function (error, stdout, stderr) {
+      if (error) logger("error", "proj42wkt failure:", "server", null, error);
+      res.send(stdout);
+    }
+  );
 });
 
 module.exports = router;
