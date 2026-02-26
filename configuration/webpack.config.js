203e62814e5b7bb55f956e894c33d06d599cd0ed
ac-61 <ac-61@users.noreply.github.com>
2026-02-26 12:52:42 -0800

    Add font types as asset to webpack (#874)
    * Add font types as asset to webpack
    
    * chore: bump version to 4.2.12-20260226 [version bump]
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- configuration/webpack.config.js
+++ configuration/webpack.config.js
@@ -63,6 +63,8 @@ const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
+const fontRegex = /\.(eot|woff|woff2|ttf)$/;
+
 
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
@@ -492,6 +494,10 @@ module.exports = function (webpackEnv) {
                 },
                 "sass-loader"
               ),
+            },
+            {
+              test: fontRegex,
+              type: 'asset/resource',
             },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
