9018fc5af46a926c49cbe39b0313831e806af6e5
ac-61 <ac-61@users.noreply.github.com>
2026-04-13 16:11:51 -0700

    Add option to set tool to open by default (#923)
    * Add option to set tool to open by default
    
    * chore: bump version to 4.2.37-20260406 [version bump]
    
    * Account for tools that are turned off
    
    * Remomve unused items
    
    * Add None as option
    
    * Bump versions
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- configure/src/core/Maker.js
+++ configure/src/core/Maker.js
@@ -1314,6 +1314,67 @@ const getComponent = (
           )}
         </div>
       );
+    case "defaulttooldropdown":
+      let tools =  configuration?.tools || null
+      tools = tools.filter(tool => {
+        return tool?.separatedTool !== true && tool?.on !== false
+      }).map(tool => tool.name)
+
+      tools.unshift("None")
+
+      inner = (
+        <FormControl className={c.dropdown} variant="filled" size="small">
+          <InputLabel>{com.name}</InputLabel>
+          <Select
+            disabled={disabled || isDisabled}
+            value={value || getIn(directConf, com.field, tools[0])}
+            onChange={(e) => {
+              if (!isDisabled) {
+                updateConfiguration(
+                  forceField || com.field,
+                  e.target.value,
+                  layer
+                );
+              }
+            }}
+          >
+            {tools.map((o) => {
+              return (
+                <MenuItem value={o}>
+                  {typeof o === "string" ? o.toUpperCase() : o}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+      );
+      return (
+        <div style={isDisabled ? { opacity: 0.5 } : {}}>
+          {inlineHelp ? (
+            <>
+              {inner}
+              <div
+                className={c.subtitle2}
+                dangerouslySetInnerHTML={{ __html: isDisabled
+                  ? `${com.description || ""}\n\nNote: ${disabledMessage}`
+                  : com.description || "" }}
+              ></div>
+            </>
+          ) : (
+            <Tooltip
+              title={
+                isDisabled
+                  ? `${com.description || ""}\n\nNote: ${disabledMessage}`
+                  : com.description || ""
+              }
+              placement="top"
+              arrow
+            >
+              {inner}
+            </Tooltip>
+          )}
+        </div>
+      );
     case "objectarray":
       const section = [];
       let items;
